b141328e8836aaa6921c5302e4176bbdf74b6e0d
admin <admin@galliumstudio.com>
2022-08-27 01:32:24 -0700

    Updated copyright notice.

--- Src/app/Demo/Demo.cpp
+++ Src/app/Demo/Demo.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) Gallium Studio LLC. All rights reserved.
+ * Copyright (C) 2018 Gallium Studio LLC (Lawrence Lo). All rights reserved.
  *
  * This program is open source software: you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as published
